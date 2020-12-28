c24438beea5d18ce1a497d7f0130081c2e917bc0
xiaannng <xiang.yan@alumni.ubc.ca>
2020-12-27 22:29:48 -0800

    Initial Commit

---
+++ singlePhoto/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  singlePhoto
+//
+//  Created by Xiang Yan on 2020-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ singlePhoto/singlePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  singlePhotoApp.swift
+//  singlePhoto
+//
+//  Created by Xiang Yan on 2020-12-27.
+//
+
+import SwiftUI
+
+@main
+struct singlePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
